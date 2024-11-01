cebc8fbefe6da468bb968a47e5d27c957f868c0e
Radu Gheorghe <radu2002ro@gmail.com>
2024-11-01 19:48:32 +0200

    feat: add models for user, question, answer and vote

---
+++ src/models/user-entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { Question } from './question-entity';
+import { Answer } from './answer-entity';
+import { Vote } from './vote-entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column({ length: 100 })
+  displayName!: string;
+
+  @OneToMany(() => Question, (question) => question.author, { nullable: true })
+  questions?: Question[];
+
+  @OneToMany(() => Answer, (answer) => answer.author, { nullable: true })
+  answers?: Answer[];
+
+  @OneToMany(() => Vote, (vote) => vote.user, { nullable: true })
+  votes?: Vote[];
+
+  @CreateDateColumn()
+  created: Date;
+}
